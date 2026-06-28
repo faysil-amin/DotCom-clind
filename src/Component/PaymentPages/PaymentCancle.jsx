7ea4b18a5ff30671ad4431695263f2d5d2c323f8
Nihad Bhuiyan <nihadbhuiyanb@gmail.com>
2026-06-28 15:38:59 +0600

    add payment cancle page

---
+++ src/Component/PaymentPages/PaymentCancle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PaymentCancle() {
+    return (
+        <div>
+            payment cancle
+        </div>
+    )
+}
--- src/Component/PaymentPages/PaymentSuccessFull.jsx
+++ src/Component/PaymentPages/PaymentSuccessFull.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PaymentSuccessFull() {
     return (
         <div>
-
+            payment successfull
         </div>
     )
 }
